b4dd8161522ac3f3f8e905f8f7cf81a5e263a7eb
d-captainkenya <dcaptainkenya@gmail.com>
2022-05-17 16:25:53 +0300

    introduced variables

--- first.cpp
+++ first.cpp
@@ -1,8 +1,32 @@
-#include <iostream>
+
+/*#include <iostream>
 using namespace std;
 
 int main() {
   cout << "Hello World! \n";      // \n or endl for newline
   cout << "Hey again" << endl;
   return 0;
-} 
+}*/
+
+
+
+#include <iostream>
+using namespace std;
+int main() {
+
+  int age = 27;
+  double token = 57.45;
+  char letter = 'A';
+  string hello = "Hey man";
+  bool istrue = true;
+
+  cout << age << endl << token << endl << letter << endl << hello << istrue << endl;
+  cout << "Lil bruh is " << age << " years\n";
+
+  int x = 1000;
+  int y = 457;
+  int sum = x+y;
+  cout << sum << endl;
+  
+  return 0;
+}
